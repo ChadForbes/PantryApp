e9817c0a1cee330a1b031dfce34b7efac758a5bd
Stephen Allen <staylorallen@gmail.com>
2019-10-16 11:57:39 -0400

    able to make hard coded API call with button press

--- app/src/main/java/com/mobilesoftware/pantryapp/MainActivity.java
+++ app/src/main/java/com/mobilesoftware/pantryapp/MainActivity.java
@@ -12,8 +12,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.mobilesoftware.pantryapp.ui.main.SectionsPagerAdapter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,5 +52,47 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // testing API recipe search
+        final Button APISearchButton = findViewById(R.id.button_testAPI);
+        APISearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                OkHttpClient client = new OkHttpClient();
+                String url = "https://www.food2fork.com/api/search?key=1e57f850de5ad1ed723a26fdb35ca897&q=chicken&count=1";
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        if(response.isSuccessful()) {
+                            final String myResponse = response.body().string();
+
+                            MainActivity.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    System.out.println(myResponse);
+                                }
+                            });
+                        }
+                    }
+                });
+
+
+            }
+
+        });
+
     }
+
+
+
+
 }
