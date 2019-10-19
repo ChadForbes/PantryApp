214ea887144e6a765522472ebd59984148fd9da8
ZEUS\chads <ninjagamer32@gmail.com>
2019-10-19 02:17:28 -0400

    Setup most of the front end
    Having problems live refreshing the list

--- app/src/main/java/com/mobilesoftware/pantryapp/ui/main/MyPantryFragment.java
+++ app/src/main/java/com/mobilesoftware/pantryapp/ui/main/MyPantryFragment.java
@@ -1,8 +1,10 @@
 package com.mobilesoftware.pantryapp.ui.main;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -10,11 +12,7 @@ import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
+import com.mobilesoftware.pantryapp.CreateItemActivity;
 import com.mobilesoftware.pantryapp.Food;
 import com.mobilesoftware.pantryapp.FoodRepository;
 import com.mobilesoftware.pantryapp.R;
@@ -28,6 +26,8 @@ public class MyPantryFragment extends Fragment {
     private RecyclerView mrecyclerView;
     private List<Food> foodList;
     private FoodRepository foodRepository;
+    RecyclerViewAdapter recyclerAdapter;
+    Intent editIntent;
 
     public MyPantryFragment() {
         // Required empty public constructor
@@ -37,12 +37,28 @@ public class MyPantryFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        editIntent = new Intent(getContext(), CreateItemActivity.class);
+
         foodList = new ArrayList<>();
         foodRepository = new FoodRepository(getContext());
         foodRepository.getFoods().observe(this, new Observer<List<Food>>() {
             @Override
             public void onChanged(@Nullable final List<Food> foods) {
                 foodList.addAll(foods);
+                mrecyclerView = (RecyclerView) v.findViewById(R.id.foodlst);
+                recyclerAdapter = new RecyclerViewAdapter(getContext(), foodList);
+                mrecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                mrecyclerView.setAdapter(recyclerAdapter);
+                recyclerAdapter.setOnItemClickListener(new RecyclerViewAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(int position) {
+                        editIntent.putExtra("mode", "edit");
+                        editIntent.putExtra("itemId", foodList.get(position).uid);
+                        startActivity(editIntent);
+                        recyclerAdapter.notifyDataSetChanged();
+                    }
+                });
+                recyclerAdapter.notifyDataSetChanged();
             }
 
         });
@@ -51,11 +67,7 @@ public class MyPantryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-            v = inflater.inflate(R.layout.fragment_my_pantry, container, false);
-            mrecyclerView = (RecyclerView) v.findViewById(R.id.foodlst);
-            RecyclerViewAdapter recyclerAdapter = new RecyclerViewAdapter(getContext(), foodList);
-            mrecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-            mrecyclerView.setAdapter(recyclerAdapter);
-            return v;
+        v = inflater.inflate(R.layout.fragment_my_pantry, container, false);
+        return v;
     }
 }
