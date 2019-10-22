7636d1eb262767f4af4aa9ac40cc448f3f446099
Stephen Allen <staylorallen@gmail.com>
2019-10-22 14:10:19 -0400

    stable API searching

--- app/src/main/java/com/mobilesoftware/pantryapp/MainActivity.java
+++ app/src/main/java/com/mobilesoftware/pantryapp/MainActivity.java
@@ -49,17 +49,47 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
 
-        Food food = new Food();
-        food.name = "Salmon";
-        food.aliases = "";
-        food.amount = 16;
-        food.amountType = "lb";
+//        Food food = new Food();
+//        food.name = "Beef";
+//        food.aliases = "";
+//        food.amount = 16;
+//        food.amountType = "lb";
+//        food.searchable = true;
+//
+//        foodRepository.insertFood(food);
+//
+//        Food food2 = new Food();
+//        food2.name = "Chicken";
+//        food2.aliases = "";
+//        food2.amount = 16;
+//        food2.amountType = "lb";
+//        food2.searchable = true;
+//
+//        foodRepository.insertFood(food2);
+//
+//        Food food3 = new Food();
+//        food3.name = "Spinach";
+//        food3.aliases = "";
+//        food3.amount = 16;
+//        food3.amountType = "lb";
+//        food3.searchable = true;
+//
+//        foodRepository.insertFood(food3);
+//
+//        Food food4 = new Food();
+//        food4.name = "Honey";
+//        food4.aliases = "";
+//        food4.amount = 16;
+//        food4.amountType = "lb";
+//        food4.searchable = true;
 
-        foodRepository.insertFood(food);
+//        foodRepository.insertFood(food4);
+
+
         final LifecycleOwner owner = this;
 
         Food newFood;
- 
+
         foodRepository.getFoods().observe(this, new Observer<List<Food>>() {
             @Override
             public void onChanged(@Nullable final List<Food> foods) {
